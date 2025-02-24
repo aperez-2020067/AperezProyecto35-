461f51b096117e1613e919d6ea317edf343a99c3
aperez-2020067} <aperez-2020067@kinal.edu.gt>
2025-02-23 19:30:18 -0600

    Se agrega Modelo de carrito,Modelo de Invoice

---
+++ src/cart/cart.model.js
@@ -0,0 +1,14 @@
+
+import { Schema, model } from 'mongoose';
+
+const cartSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+    {
+      productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true, min: 1 },
+    },
+  ],
+}, { timestamps: true });
+
+export default model('Cart', cartSchema);
